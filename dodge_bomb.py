36532c925b64076b45ddd61aed7974bf241b6552
C0B22064 <c0b2206400@edu.teu.ac.jp>
2023-04-25 15:44:02 +0900

    練習6

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,6 +10,23 @@ delta = {
     pg.K_RIGHT: (+1, 0)
 }
 
+
+def cheack_bound(scr_rct:pg.Rect, obj_rct:pg.Rect):
+    """
+    オブジェクトが画面内OR画面外を判定し　真理値タプルを返す
+    引数１ :画面SarfaceのRect
+    引数２ ：こうかとんか爆弾の
+    戻り値 :横縦の判定結果
+    """
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -36,18 +53,31 @@ def main():
                 return 0
 
         tmr += 1
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rect)
+        
 
         key_lst = pg.key.get_pressed()
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rect.move_ip((mv))  # keyに応じてこうかとんを動かす
-
+                if cheack_bound(screen.get_rect(), kk_rect) != (True, True):
+                    for k, mv in delta.items():
+                        if key_lst[k]:
+                            kk_rect.move_ip((-mv[0], -mv[1]))
+        
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, kk_rect)
         bb_rect.move_ip(vx, vy)  #爆弾を動かす
+        yoko, tate = cheack_bound(screen.get_rect(), bb_rect)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        
         screen.blit(bb_img, bb_rect)
+        if kk_rect.colliderect(bb_rect):
+            return
 
-
+        screen.blit(bb_img, bb_rect)
         pg.display.update()
         clock.tick(1000)
 
