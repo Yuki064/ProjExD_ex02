b9a9e23ea2c6930614eeee783879157a9f4d0635
C0B22064 <c0b2206400@edu.teu.ac.jp>
2023-04-25 18:31:13 +0900

    演習2完了#Issue1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -31,6 +31,8 @@ def cheack_bound(scr_rct:pg.Rect, obj_rct:pg.Rect):
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
+   
+
     
     clock = pg.time.Clock()
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
@@ -57,26 +59,44 @@ def main():
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 爆弾作成
     bb_img.set_colorkey((0, 0, 0))
     x, y = random.randint(0,1600), random.randint(0,900)
-    #screen.blit(bb_img, [x,y])
-    vx,vy=+1,-1
+    # screen.blit(bb_img, [x,y])
+    bb_vx,bb_vy = +1,-1
     bb_rect = bb_img.get_rect()
     bb_rect.center = x, y
     kk_rect = kk_img.get_rect()
     kk_rect.center = 900, 400
 
+    gameover = False
+    tmr_go = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return 0
-
-        tmr += 1
         
-        #idou = []
+        tmr += 1
+        print(tmr - tmr_go)
+        if gameover == True:
+            screen.blit(kk_img_go, kk_rect)
+            if tmr-tmr_go >= 3000:
+                return
+            continue
+        
+        accs = [a for a in range(1,11)]  # 加速度リスト
+        bb_imgs = []
+        for r in range(1, 11):  # 拡大爆弾
+            bb_img = pg.Surface((20*r, 20*r))
+            pg.draw.circle(bb_img, (255,0,0), (10*r,10*r), 10*r)
+            bb_img.set_colorkey((0, 0, 0))
+            bb_imgs.append(bb_img)
+        
+        avx, avy = bb_vx*accs[min(tmr//1000, 9)], bb_vy*accs[min(tmr//1000, 9)]
+        bb_img = bb_imgs[min(tmr//1000, 9)]
+        bb_rect.move_ip(avx, avy)  # 変化後の爆弾を動かす
 
         key_lst = pg.key.get_pressed()
         for k, mv in delta.items():
             if key_lst[k]:
-                kk_rect.move_ip((mv))  #keyに応じてこうかとんを動かす
+                kk_rect.move_ip((mv))  # keyに応じてこうかとんを動かす
                 
                 if cheack_bound(screen.get_rect(), kk_rect) != (True, True):  #こうかとんが画面外にいかないように
                     for k, mv in delta.items():
@@ -85,20 +105,24 @@ def main():
         
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rect)
-        
-        bb_rect.move_ip(vx, vy)  # 爆弾を動かす
+        # screen.blit(kk_imgs[mv], kk_rect)
+        bb_rect.move_ip(bb_vx, bb_vy)  # 爆弾を動かす
         yoko, tate = cheack_bound(screen.get_rect(), bb_rect)  # 爆弾が画面から出ないように
         if not yoko:
-            vx *= -1
+            bb_vx *= -1
         if not tate:
-            vy *= -1
+            bb_vy *= -1
         
         screen.blit(bb_img, bb_rect)
+        
         if kk_rect.colliderect(bb_rect):  # 衝突
-            screen.blit(kk_img_go, kk_rect)
-            return
+            gameover = True
+            tmr_go = tmr
+        
+                
+        
 
-        screen.blit(bb_img, bb_rect)
+        #screen.blit(bb_img, bb_rect)
         pg.display.update()
         clock.tick(1000)
 
